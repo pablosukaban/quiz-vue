cbec6ed37c0cccfd53b1b4517d704ac1ead10bdb
Ilya Prikhodko <zapaska96@gmail.com>
2025-05-04 13:14:06 +0500

    add primevue

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import Primevue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Primevue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.mount('#app')
